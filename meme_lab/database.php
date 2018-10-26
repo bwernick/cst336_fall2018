4f7e9131a2868d26d5b82db73a1d490d9c7fcb49
Bradley Wernick <bwernick@csumb.edu>
2018-10-26 20:33:56 +0000

    db suffering

--- meme_lab/database.php
+++ meme_lab/database.php
@@ -2,11 +2,18 @@
 // connect to our mysql database server
 
 function getDatabaseConnection() {
-    $host = "localhost";
-    $username = "bradley";
-    $password = "cst336";
-    $dbname = "meme_lab"; 
-    
+    if ($_SERVER['SERVER_NAME'] == "cst336-fall-2018-uuts.c9users.io") { // running on cloud9
+        $host = "localhost";
+        $username = "bradley";
+        $password = "cst336";
+        $dbname = "meme_lab"; 
+    }else{
+        //JAWSDB (faster than ClearDB)
+        $host = "tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $username = "f89d4v70t1c7x38p";
+        $password = "f3qapnhmsbtvmehi";
+        $dbname = "q04pglwa9t4hfqjm"; 
+    }
     // Create connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
